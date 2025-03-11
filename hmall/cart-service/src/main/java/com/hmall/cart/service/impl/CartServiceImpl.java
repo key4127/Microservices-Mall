79a84a9c3e870f13ae65e7532faf6df45a252216
key4127 <2827092384@qq.com>
2025-03-11 08:41:47 +0800

    configure nacos

--- hmall/cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
+++ hmall/cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
@@ -1,6 +1,7 @@
 package com.hmall.cart.service.impl;
 
 import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.RandomUtil;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -15,6 +16,8 @@ import com.hmall.common.utils.BeanUtils;
 import com.hmall.common.utils.CollUtils;
 import com.hmall.common.utils.UserContext;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -40,6 +43,8 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements ICartService {
     private final RestTemplate restTemplate;
+
+    private final DiscoveryClient discoveryClient;
 
     // private final IItemService itemService;
 
@@ -84,9 +89,14 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
     private void handleCartItems(List<CartVO> vos) {
         // 1.获取商品id息
         Set<Long> itemIds = vos.stream().map(CartVO::getItemId).collect(Collectors.toSet());
-        // 2.1.利用RestTemplate发起http请求，得到http的响应
+        // 2.查询
+        // 2.1 发现服务实例列表
+        List<ServiceInstance> instances = discoveryClient.getInstances("item-service");
+        // 2.2 负载均衡
+        ServiceInstance instance = instances.get(RandomUtil.randomInt(instances.size()));
+        // 2.3 请求
         ResponseEntity<List<ItemDTO>> response = restTemplate.exchange(
-                "http://localhost:8081/items?ids={ids}",
+                instance.getUri() + "/items?ids={ids}",
                 HttpMethod.GET,
                 null,
                 new ParameterizedTypeReference<List<ItemDTO>>() {
